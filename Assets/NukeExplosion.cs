6ddbc44dbc79ca826356c833fb40140a8a271371
RazorTag <enorswo@gmail.com>
2014-12-03 00:53:19 -0500

    deleted unnecessary scenes

---
+++ Assets/NukeExplosion.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NukeExplosion : Torpedo {
+	public static readonly float heavyDamage = 3f;
+	public static readonly float heavyStun = 0f;
+	
+	// Use this for initialization
+	void Start () {
+		stun = heavyStun;
+		damage = heavyDamage;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
